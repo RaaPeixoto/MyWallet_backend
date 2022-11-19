4c488fd3936c9a2c99245b063d036d9e8f90e8ca
Raísa Peixoto <raisacad.engcivil@gmail.com>
2022-11-19 19:13:42 -0300

    "add: put transactions"

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 export async function getTransactions(req, res) {
   //"a4f79fd4-7786-40ed-a778-0ca544efc613"
   const { authorization } = req.headers;
-    // validar
+  // validar
   const token = authorization?.replace("Bearer ", "");
   if (!token) {
     return res.sendStatus(401);
@@ -12,9 +12,11 @@ export async function getTransactions(req, res) {
   try {
     const sessions = await sessionsCollection.findOne({ token });
     console.log(sessions);
-    const userTransactions = await transactionsCollection.find({
-      userId: sessions?.userId,
-    }).toArray();
+    const userTransactions = await transactionsCollection
+      .find({
+        userId: sessions?.userId,
+      })
+      .toArray();
 
     res.send(userTransactions);
   } catch (err) {
@@ -26,7 +28,7 @@ export async function getTransactions(req, res) {
 export async function postTransaction(req, res) {
   //"a4f79fd4-7786-40ed-a778-0ca544efc613"
   //{"value":22,"description":"teste","type":"deposit"}
-  const { value, description,type } = req.body;
+  const { value, description, type } = req.body;
   // validar
   const { authorization } = req.headers;
   const today = dayjs().format("DD/MM/YY");
@@ -39,25 +41,24 @@ export async function postTransaction(req, res) {
     const session = await sessionsCollection.findOne({ token });
     if (!session) {
       return res.sendStatus(401);
-  }
+    }
     console.log(session);
     await transactionsCollection.insertOne({
       userId: session?.userId,
       description,
       value,
-      date:today,
+      date: today,
       type,
     });
-    
+
     res.sendStatus(200);
-    
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
   }
 }
 
-export async function deleteTransaction(req,res){
+export async function deleteTransaction(req, res) {
   const { authorization } = req.headers;
   const { id } = req.params;
   const token = authorization?.replace("Bearer ", "");
@@ -67,7 +68,7 @@ export async function deleteTransaction(req,res){
   const session = await sessionsCollection.findOne({ token });
   if (!session) {
     return res.sendStatus(401);
-}
+  }
   const existingTransaction = await transactionsCollection.findOne({
     _id: new ObjectId(id),
   });
@@ -75,12 +76,11 @@ export async function deleteTransaction(req,res){
     return res.sendStatus(404);
   }
 
-  console.log(session.userId)
-  if(existingTransaction.userId.toString() !== session.userId.toString() ){
+  console.log(session.userId);
+  if (existingTransaction.userId.toString() !== session.userId.toString()) {
     return res.sendStatus(401);
   }
   try {
-   
     await transactionsCollection.deleteOne({
       _id: existingTransaction._id,
     });
@@ -90,5 +90,48 @@ export async function deleteTransaction(req,res){
     console.log(error);
     res.sendStatus(500);
   }
+}
 
+export async function updateTransaction(req, res) {
+  const { description, value } = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  const { id } = req.params;
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  const session = await sessionsCollection.findOne({ token });
+  if (!session) {
+    return res.sendStatus(401);
+  }
+
+  if (!session) {
+    return res.sendStatus(401);
+  }
+  const existingTransaction = await transactionsCollection.findOne({
+    _id: new ObjectId(id),
+  });
+  if (!existingTransaction) {
+   
+    return res.sendStatus(404);
+  }
+  if (existingTransaction.userId.toString() !== session.userId.toString()) {
+    return res.sendStatus(401);
+  }
+
+  try {
+    await transactionsCollection.updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      {
+        $set: { value, description },
+      }
+    );
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
--- src/routers/usersRouters.js
+++ src/routers/usersRouters.js
@@ -1,7 +1,7 @@
 import { signIn, signUp } from "../controllers/usersController.js";
 import { Router } from "express";
 import { newUserValidation } from "../middlewares/newUserMiddleware.js";
-import { deleteTransaction, getTransactions, postTransaction } from "../controllers/transactionsController.js";
+import { deleteTransaction, getTransactions, postTransaction, updateTransaction } from "../controllers/transactionsController.js";
 
 const router =Router();
 
@@ -10,5 +10,6 @@ router.post("/signIn",signIn);
 router.post("/transactions",postTransaction);
 router.get("/transactions",getTransactions);
 router.delete("/transactions/:id",deleteTransaction);
+router.put("/transactions/:id",updateTransaction)
 
 export default router;
